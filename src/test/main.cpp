b50369003ab047f0ef33176694f93a5b4d4807d3
lzxm160 <lzxm160@sina.com>
2017-02-03 20:02:04 +0800

    test

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,55 +1,14 @@
 #include <boost/program_options.hpp>
-#include "log.hpp"
-#include "fastcgi/fastcgi_resource.h"
 using std::string;
 using std::cout;
 using std::endl;
-void start_inspector_thread();
-void parseCommandLine(int argc, char* argv[])
-{
-    using namespace boost::program_options;
-    //声明需要的选项
-    options_description desc("Allowed options");
-    desc.add_options()
-        ("help,h", "produce help message")
-        ("config,c", value<string>()->default_value("config.ini"), "set config")
-        ;
-
-    variables_map vm;        
-    store(parse_command_line(argc, argv, desc), vm);
-    notify(vm);    
-
-    if (vm.count("help")) 
-    {
-        cout << desc;
-    }
-   
-    if (vm.count("config"))  
-    {  
-        string conf_name = vm["config"].as<string>();  
-        //LOG_INFO<<conf_name;
-        //ifstream ifs_config(conf_name.c_str());  
-        //boost::shared_ptr<iconfig> get_config= iconfig::get_instance(conf_name);
-        //get_config= iconfig::get_instance(conf_name);
-    }  
-}
+int i=1;
 int main(int argc, char* argv[])
 {
   try
   {
-    parseCommandLine(argc, argv);
-
-    init_log();
-    muduo::Timestamp start = muduo::Timestamp::now();
-
-    start_fastcgi();
-
-    //test1::test1();
-    //short_url::test();
-    //start_inspector_thread();
-    
-    muduo::Timestamp end = muduo::Timestamp::now();
-    printf("%f\n", timeDifference(end, start));
+    int i=i;
+    printf("%d\n", i);
     sleep(10);
   }
   catch (std::exception& e)
