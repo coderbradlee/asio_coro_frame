fc339ea24475085b94834b7d88a656f2bbc035cf
lzxm160 <lzxm160@sina.com>
2017-02-04 14:32:18 +0800

    h

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
 	try
 	{
 	    HttpServer server(get_config->m_port,get_config->m_threads);
-	    serverRedisResource(server,get_config->m_redis_host,get_config->m_redis_port,"",url);
+	    serverRedisResource(server,get_config->m_redis_host,get_config->m_redis_port,"",get_config->m_url);
 	    thread server_thread([&server]()
 	    {
 	        server.start();
