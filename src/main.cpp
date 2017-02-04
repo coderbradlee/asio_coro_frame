c7dd846107fecda302f5aab1c27ba72691f4d064
lzxm160 <lzxm160@sina.com>
2017-02-04 16:41:33 +0800

    h

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include "config.hpp"
 #include "log.h"
 #include "serverResource.hpp"
-#include <boost/timer.hpp>
 int main() {
 	try
 	{
 	    HttpServer server(get_config->m_port,get_config->m_threads);
 	    serverRedisResource(server);
 
-	    boost::timer tm;
+	    boost::timer::timer tm;
 		
 	    for(int i=0;i<100000;++i)
 	    {
