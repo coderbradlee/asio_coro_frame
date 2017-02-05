60af44bd999a0417a7b614b4efebafeed52d431d
lzxm160 <lzxm160@sina.com>
2017-02-05 18:08:04 +0800

    auto-flush

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@ int main() {
 	    HttpServer server(get_config->m_port,get_config->m_threads);
 	    serverRedisResource(server);
 
-	    boost::timer::cpu_timer pass;
-		pass.start();
+	 //    boost::timer::cpu_timer pass;
+		// pass.start();
 		
-	    for(int i=0;i<1000000;++i)
-	    {
-	    	BOOST_LOG_SEV(slg, notification)<<i;
-	    }
-	    std::cout << "now time elapsed:" << pass.format(6) << std::endl;
+	 //    for(int i=0;i<1000000;++i)
+	 //    {
+	 //    	BOOST_LOG_SEV(slg, notification)<<i;
+	 //    }
+	 //    std::cout << "now time elapsed:" << pass.format(6) << std::endl;
 	    
 
 	    thread server_thread([&server]()
