a9b8193a4304066c9aec9c6c29d8307669527269
lzxm160 <lzxm160@sina.com>
2017-02-07 22:03:28 +0800

    coro

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -5,38 +5,66 @@ using std::string;
 using std::cout;
 using std::endl;
 using boost::asio::coroutine;
-class test_coro
+class server:public boost::enable_shared_from_this<server>
 {
 public:
-  test_coro():i(0){}
-  void operator()()
+  server(const string& addr,const string& port):m_io_service(),m_acceptor(m_io_service),m_socket(m_io_service)
   {
-    reenter(m_coro)
-    {
-      while(i<10)
-      {
-         std::cout<<i<<std::endl;
-         ++i;
-         yield;
-         std::cout<<i<<std::endl;
-         ++i;
-      }
-    }
+    boost::asio::ip::tcp::resolver res(m_io_service);
+    boost::asio::ip::tcp::endpoint end=*res.resolve({addr,port});
+    m_acceptor.open(end.protocol());
+    m_acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+    m_acceptor.bind(end);
+    m_acceptor.listen();
+    do_accept();
   }
+  void run()
+  {
+    m_io_service.run();
+  }
+  void do_accept()
+  {
+     reenter(this)
+     {
+        for(;;)
+        {
+          yield m_acceptor.async_accept(m_socket,
+            [this](boost::system::error_code ec)
+            {
+              if(!m_acceptor.is_open())
+              {
+                return;
+              }
+              if(!ec)
+              {
+                cout<<"acceptor"<<endl;
+              }
+            });
+        }
+       
+     }
+  }
+  
 private:
   coroutine m_coro;
-  int i;
+  boost::asio::io_service m_io_service;
+  boost::asio::ip::tcp::acceptor m_acceptor;
+  boost::asio::ip::tcp::socket m_socket;
+};
+class client
+{
+public:
+  client(){;}
+  ~client(){}
+private:
+  coroutine m_coro;
 };
 int main(int argc, char* argv[])
 {
   try
   {
-    test_coro t1,t2;
-    for(;;)
-    {
-      t1();
-      // t2();
-    }
+    server s("127.0.0.1",8000);
+    s.run();
   }
   catch (std::exception& e)
   {
