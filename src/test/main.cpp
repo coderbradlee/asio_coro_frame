e5f41085f426a3e14660acbaf45055ed4797634d
lzxm160 <lzxm160@sina.com>
2017-02-07 22:13:37 +0800

    coro

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/asio/yield.hpp>
+#include <boost/enable_shared_from_this.hpp>  
+#include <boost/shared_ptr.hpp> 
 using std::string;
 using std::cout;
 using std::endl;
