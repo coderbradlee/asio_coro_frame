c3721b8a9ed65af215971b947fb6dd26b5f5102d
lzxm160 <lzxm160@sina.com>
2017-02-08 16:32:05 +0800

    coro

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include "config.hpp"
-#include "serverResource.hpp"
+#include "server.hpp"
 int main() {
 	try
 	{
-	    HttpServer server(get_config->m_port,get_config->m_threads);
-	    serverRedisResource(server);
+	    // HttpServer server(get_config->m_port,get_config->m_threads);
+	    // serverRedisResource(server);
+		http::server::server s("0.0.0.0",get_config->m_port,"web");
+    	s.run();
 
+    	
 	    boost::timer::cpu_timer pass;
 		pass.start();
 		
-	    for(int i=0;i<900000;++i)
-	    {
-	    	BOOST_LOG_SEV(slg, notification)<<i;
-	    }
+	    // for(int i=0;i<900000;++i)
+	    // {
+	    // 	BOOST_LOG_SEV(slg, notification)<<i;
+	    // }
 	    std::cout << "now time elapsed:" << pass.format(6) << std::endl;
 	    // initsink->flush();
 
