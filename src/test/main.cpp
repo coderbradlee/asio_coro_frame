40e8dda4876398185925abe933efa250eaa4eeab
lzxm160 <lzxm160@sina.com>
2017-02-08 14:41:06 +0800

    coro

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -8,63 +8,13 @@ using std::string;
 using std::cout;
 using std::endl;
 using boost::asio::coroutine;
-class server:public boost::enable_shared_from_this<server>
-{
-public:
-  server(const string& addr,const string& port):m_io_service(),m_acceptor(m_io_service),m_socket(m_io_service)
-  {
-    boost::asio::ip::tcp::resolver res(m_io_service);
-    boost::asio::ip::tcp::endpoint end=*res.resolve({addr,port});
-    m_acceptor.open(end.protocol());
-    m_acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
-    m_acceptor.bind(end);
-    m_acceptor.listen();
-    do_accept();
-  }
-  void run()
-  {
-    m_io_service.run();
-  }
-  void do_accept()
-  {
-     // reenter(m_coro)
-     // {
-     //    yield m_acceptor.async_accept(m_socket,
-     //        [this](boost::system::error_code ec)
-     //        {
-     //          if(!m_acceptor.is_open())
-     //          {
-     //            return;
-     //          }
-     //          if(!ec)
-     //          {
-     //            cout<<"acceptor"<<endl;
-     //          }
-     //          do_accept();
-     //        });     
-     // }
-  }
-  
-private:
-  coroutine m_coro;
-  boost::asio::io_service m_io_service;
-  boost::asio::ip::tcp::acceptor m_acceptor;
-  boost::asio::ip::tcp::socket m_socket;
-};
-class client
-{
-public:
-  client(){;}
-  ~client(){}
-private:
-  coroutine m_coro;
-};
+
 int main(int argc, char* argv[])
 {
   try
   { 
     test();
-    
+
   }
   catch (std::exception& e)
   {
--- src/test/test.hpp
+++ src/test/test.hpp
@@ -9,67 +9,79 @@ using std::string;
 using std::cout;
 using std::endl;
 using boost::asio::coroutine;
-class test_strand
+class test_strand:public boost::enable_shared_from_this<test_strand>
 {
 public:
-  test_strand(boost::asio::io_service& m_io_service):m_strand(m_io_service),
-  m_timer1(m_io_service, boost::posix_time::seconds(1)),
-  m_timer2(m_io_service, boost::posix_time::seconds(1)),
-  m_count(0)
+  test_strand(const string& addr,const string& port):m_io_service(),m_strand(m_io_service),m_acceptor(m_io_service),m_socket(m_io_service)
   {
-    m_timer1.async_wait(m_strand.wrap(boost::bind(&test_strand::print1,this)));
-    m_timer2.async_wait(m_strand.wrap(boost::bind(&test_strand::print2,this)));
+    boost::asio::ip::tcp::resolver res(m_io_service);
+    boost::asio::ip::tcp::endpoint end=*res.resolve({addr,port});
+    m_acceptor.open(end.protocol());
+    m_acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+    m_acceptor.bind(end);
+    m_acceptor.listen();
+    do_accept();
+    
+  }
+  void do_accept()
+  {
+    m_acceptor.async_accept(m_socket,[this](boost::system::error_code ec)
+      {
+        if(!m_acceptor.is_open())
+        {
+          cout<<"acceptor fail"<<endl;
+          return;
+        }
+        if(!ec)
+        {
+          boost::asio::spawn(m_strand,do_some);
+        }
+      });
+    
+  }
+  void do_some(boost::asio::yield_context yield)
+  {
+    try
+    {
+      std::vector<char> v(128,0);
+      for(;;)
+      {
+        size_t len=m_socket.async_read_some(boost::asio::buffer(data),yield);
+        boost::asio::async_write(m_socket,boost::asio::buffer(data,len),yield);
+      }
+    }
+    catch(std::exception& e)
+    {
+      cout<<e.what()<<endl;
+    }
   }
   ~test_strand()
   {
     cout<<"last:"<<m_count<<endl;
   }
+  void run()
+  {
+    boost::thread_group threads;
+    for (int i = 0; i < 3; ++i)
+        threads.create_thread(boost::bind(&boost::asio::io_service::run,&m_io_service));
+    m_io_service.run();
+    threads.join_all();
+  }
   
-  void print1()
-  {
-    if(m_count<10)
-    {
-      cout<<"timer 1:"<<m_count<<endl;
-      ++m_count;
-      m_timer1.expires_at(m_timer1.expires_at()+boost::posix_time::seconds(1));
-      m_timer1.async_wait(m_strand.wrap(boost::bind(&test_strand::print1,this)));
-    }
-  }
-  void print2()
-  {
-    if(m_count<10)
-    {
-      cout<<"timer 2:"<<m_count<<endl;
-      ++m_count;
-      m_timer2.expires_at(m_timer2.expires_at()+boost::posix_time::seconds(1));
-      m_timer2.async_wait(m_strand.wrap(boost::bind(&test_strand::print2,this)));
-    }
-  }
 private:
-  boost::asio::io_service::strand m_strand;
-  boost::asio::deadline_timer m_timer1;
-  boost::asio::deadline_timer m_timer2;
+  coroutine m_coro;
+  boost::asio::io_service m_io_service;
+  boost::asio::ip::tcp::acceptor m_acceptor;
+  boost::asio::ip::tcp::socket m_socket;
   int m_count;
 };
 void test1()
 {
-  boost::asio::io_service io;
-  // auto p=boost::make_shared<test_strand>(io);
-  test_strand t(io);
-  boost::thread_group threads;
-  for (int i = 0; i < 3; ++i)
-      threads.create_thread(boost::bind(&boost::asio::io_service::run,&io));
-  io.run();//main thread
-  threads.join_all();
+  test_strand t("127.0.0.1","8000");
+  t.run();
 }
-void test2()
-{
-  std::vector<int> v(1);
-  std::vector<int>::iterator i = v.begin();
-  v.clear(); // invalidates iterators
-  *i = 0; // assertion!
-}
+
 void test()
 {
-  test2();
+  test1();
 }
