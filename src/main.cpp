d85408054e586558bf50577e751fdc7776e5b06d
lzxm160 <lzxm160@sina.com>
2017-02-09 16:42:28 +0800

    coro

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "config.hpp"
-#include "server.hpp"
+#include "server_http.hpp"
 int main() 
 {
 	try
 	{
-	    // HttpServer server(get_config->m_port,get_config->m_threads);
-	    // serverRedisResource(server);
-		http::server::server s("0.0.0.0",get_config->m_port,"web",7);
-    	s.run();
-
+	    HttpServer server(get_config->m_port,get_config->m_threads);
+	    serverRedisResource(server);
+		thread server_thread([&server](){
+	        //Start server
+	        server.start();
+	    });
 
 	    boost::timer::cpu_timer pass;
 		pass.start();
