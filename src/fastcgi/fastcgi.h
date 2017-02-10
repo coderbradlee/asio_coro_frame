a739a37805e7b68c70d4d3636f1b084f6ff9d85e
lzxm160 <lzxm160@sina.com>
2017-02-10 15:34:03 +0800

    asio

--- src/fastcgi/fastcgi.h
+++ src/fastcgi/fastcgi.h
@@ -19,31 +19,31 @@ public:
     explicit redis_api()
     {
         FCGX_Init();
-        FCGX_InitRequest( &request, 0, 0 );
+        FCGX_InitRequest( &m_request, 0, 0 );
         m_cluster_p = HiredisCommand<ThreadPoolCluster>::createCluster(get_config->m_redis_host.c_str(),get_config->m_redis_port);
     }
     void run()
     {
         for(;;)
         {
-            int rc = FCGX_Accept_r( &request );
+            int rc = FCGX_Accept_r( &m_request );
             
             if (rc < 0)
                 continue;
 
-            FCGX_FPrintF( request.out,
+            FCGX_FPrintF( m_request.out,
                 "Content-Type: application/xml; charset=UTF-8\r\n"
                 "Content-Encoding: gzip\r\n"
                 "\r\n"
             );
             do_redis_read();
-            int num_bytes_written = FCGX_PutStr( m_test.c_str(), m_test.length(), request.out );
+            int num_bytes_written = FCGX_PutStr( m_test.c_str(), m_test.length(), m_request.out );
             if( num_bytes_written != m_test.length() || num_bytes_written == -1 )
             {
                 break;
             }
 
-            FCGX_Finish_r( &request );
+            FCGX_Finish_r( &m_request );
         }
     }
     void do_redis_read()
