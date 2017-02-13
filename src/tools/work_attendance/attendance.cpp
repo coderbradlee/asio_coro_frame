a59eea33e45f8e69ec32101f5101cb2e3a594787
lzxm160 <lzxm160@sina.com>
2017-02-13 17:23:40 +0800

    xsl convert

--- src/tools/work_attendance/attendance.cpp
+++ src/tools/work_attendance/attendance.cpp
@@ -47,10 +47,10 @@ void month_report::write_to_excel()
   
   	for(const auto& i:*m_data)
 	{
-		*xlsxfile<<x->group;
-	 	*xlsxfile<<x->id;
-	 	*xlsxfile<<x->name;
-	 	*xlsxfile<<(x->dates).substr(0,(x->dates).size()-1);
+		*m_writer<<x->group;
+	 	*m_writer<<x->id;
+	 	*m_writer<<x->name;
+	 	*m_writer<<(x->dates).substr(0,(x->dates).size()-1);
 		m_writer->NextRow();
 	}); 
 }
