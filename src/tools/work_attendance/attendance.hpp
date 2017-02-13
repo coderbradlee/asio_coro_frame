8e2fed4caa3740f5dc18b3d75886c4877b256166
lzxm160 <lzxm160@sina.com>
2017-02-13 14:51:26 +0800

    xsl convert

--- src/tools/work_attendance/attendance.hpp
+++ src/tools/work_attendance/attendance.hpp
@@ -2,43 +2,7 @@
 #define	ATTENDANCE_HPP
 
 #include "reader_writer.hpp"
-struct report_data
- {
- 	std::string group;
- 	std::string number;
- 	std::string id;
- 	std::string name;
- 	std::string dates;
- 	std::string on_duty;
- 	std::string on_duty_desc;
- 	std::string off_duty;
- 	std::string off_duty_desc;
- 	std::string come_late;
- 	std::string leave_early;
- 	std::string extra_work;
- 	std::string away_from_work;
- 	std::string without_clock_in;
-  	
- 	void print()
- 	{
- 		std::cout<<
- 		group<<","<<
-	 	number<<","<<
-	 	id<<","<<
-	 	name<<","<<
-	 	dates<<","<<
-	 	on_duty<<","<<
-	 	on_duty_desc<<","<<
-	 	off_duty<<","<<
-	 	off_duty_desc<<","<<
-	 	come_late<<","<<
-	 	leave_early<<","<<
-	 	extra_work<<","<<
-	 	away_from_work<<","<<
-	 	without_clock_in
- 		<<std::endl;
- 	}
- }; 
+
 
 class month_report
 {
