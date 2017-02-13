8f4841f0bc7ed6c03cfc8d194737230230c3c229
lzxm160 <lzxm160@sina.com>
2017-02-13 12:29:58 +0800

    xsl convert

--- src/tools/work_attendance/attendance.hpp
+++ src/tools/work_attendance/attendance.hpp
@@ -116,7 +116,7 @@ public:
 	}
     void list_sheets()
     {
-    	xlsxioread_list_sheets(xlsxioread, boost::bind(&XLSXIOReader::xlsx_list_sheets_callback,this,_1,_2), &sheetdata);
+    	xlsxioread_list_sheets(xlsxioread, boost::bind(&XLSXIOReader::xlsx_list_sheets_callback,this,_1), &sheetdata);
     }
 	
     void process_data()
