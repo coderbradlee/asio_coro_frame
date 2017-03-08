0fe922e28856e2f125b9a05dfb3d3105b94316ad
lzxm160 <lzxm160@sina.com>
2017-03-08 15:51:07 +0800

    pdf

--- src/fastcgi/pdf_api.cpp
+++ src/fastcgi/pdf_api.cpp
@@ -8,7 +8,7 @@ void pdf_api::get_request_content(const FCGX_Request & request)
 {
     char * content_length_str = FCGX_GetParam("CONTENT_LENGTH",
                                                request.envp);
-    BOOST_LOG_SEV(slg, notification)<<__LINE__<<":content_length:"<<*content_length_str;
+    
     unsigned long content_length = 0;
 
     if (content_length_str) 
@@ -16,6 +16,7 @@ void pdf_api::get_request_content(const FCGX_Request & request)
         content_length = strtol(content_length_str,
                                 &content_length_str,
                                 10);
+        BOOST_LOG_SEV(slg, notification)<<__LINE__<<":content_length:"<<*content_length_str;
     } 
     else 
     {
